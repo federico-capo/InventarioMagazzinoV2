3247efa9998bc36eaaeacac0facd127ca9d98f86
Federico Caporuscio <126673308+federico-capo@users.noreply.github.com>
2025-03-06 04:17:11 +0100

    Add files via upload

---
+++ SourceCode/BoweInventarioV2/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace BoweInventarioV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Sans Serif Collection", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(12, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(332, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Apri file csv del portale";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Sans Serif Collection", 9.749998F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 186);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(332, 214);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(100, 412);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BoweInventarioV2.Properties.Resources.Logo_BOEWE_SYSTEC_600x180;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(332, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(362, 522);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "BoweinventarioV2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SourceCode/BoweInventarioV2/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//per leggere il file INI
+using IniParser.Model;
+using IniParser;
+
+
+//libreria execl
+using OfficeOpenXml;
+using LicenseContext = OfficeOpenXml.LicenseContext;
+
+namespace BoweInventarioV2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        //Lettura File INI
+        string LeggiValoreDaIni(string percorsoFileIni, string sezione, string chiave)
+        {
+            var parser = new FileIniDataParser();
+            if (!File.Exists(percorsoFileIni))
+            {
+                MessageBox.Show("File di configurazione non trovato!");
+                return null;
+            }
+
+            IniData data = parser.ReadFile(percorsoFileIni);
+            return data[sezione][chiave];
+        }
+
+        //creazione dizionario e funzione estrapolazione dati
+        static Dictionary<string, (string nome, string valore)> EstraiChiaviValoriDaExcel(string filePath, string sheetName)
+        {
+            var risultati = new Dictionary<string, (string nome, string valore)>();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var package = new ExcelPackage(new FileInfo(filePath)))
+
+            {
+                var worksheet = package.Workbook.Worksheets[sheetName];
+                if (worksheet == null) return risultati;
+
+                int rowCount = worksheet.Dimension.Rows;
+
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    var chiave = worksheet.Cells[row, 1].Text.Trim();
+                    var nome = worksheet.Cells[row, 2].Text;
+                    var valore = worksheet.Cells[row, 4].Text;
+
+                    if (!string.IsNullOrEmpty(chiave))
+                    {
+                        risultati[chiave] = (nome, valore);
+                    }
+                }
+            }
+            return risultati;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Apri il file Inventario con OpenFileDialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files (*.xlsx)|*.xlsx|All Files (*.*)|*.*";
+            openFileDialog.Title = "Seleziona il file csv del magazzino";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //salvataggio percorso inventario preso dal dialogfile
+                string percorsoInventario = openFileDialog.FileName;
+                //salvo sulla variabile dizionario contenuto excel
+                var dizionario = EstraiChiaviValoriDaExcel(percorsoInventario, "Inventario");
+
+
+                //manda a schermo il dizionario di string string con un ciclo for
+                textBox1.Clear();
+                foreach (var kvp in dizionario)
+                {
+                    textBox1.AppendText($"{kvp.Key}: {kvp.Value}{Environment.NewLine}");
+                }
+
+                // Leggere il percorso del secondo Excel dal file .ini
+                string percorsoFileIni = Path.Combine(Application.StartupPath, "config.ini");
+                string percorsoSecondoExcelOriginale = LeggiValoreDaIni(percorsoFileIni, "Impostazioni", "PercorsoSecondoExcel");
+
+                // Creare il percorso della copia
+                string percorsoSecondoExcelCopia = percorsoSecondoExcelOriginale + "_agg.xlsx";
+                //richiamo la funzione  aggiorno excel e gli passo i percorsi
+                AggiornaSecondoExcel(percorsoSecondoExcelOriginale, percorsoSecondoExcelCopia, "Sheet1", dizionario);
+               
+                //gestisto eccezione ini non configurato
+                if (string.IsNullOrEmpty(percorsoSecondoExcelOriginale))
+                {
+                    MessageBox.Show("Errore: il percorso del secondo file Excel non è presente nel file INI!");
+                    return;
+                }
+                //Aggiorno Textbox e Label 
+                // MessageBox.Show($"Inventario Aggiornato con successo \nExcel si trova qui: {percorsoSecondoExcelCopia}");
+                textBox1.Clear();
+                textBox1.AppendText("Inventario Aggiornato con successo Excel si trova qui\n" + percorsoSecondoExcelCopia);
+                label1.Text = "Csv\nAggiornato";
+            }
+        }
+
+        static List<string> GeneraVariantiDiChiave(string chiave)
+        {
+            var varianti = new HashSet<string> { chiave };
+
+            if (chiave.Contains(" "))
+            {
+                string secondaParte = chiave.Substring(chiave.IndexOf(" ") + 1);
+                varianti.Add(secondaParte);
+            }
+
+            if (chiave.All(char.IsDigit))
+            {
+                varianti.Add(chiave.TrimStart('0'));
+            }
+
+            if (chiave.StartsWith("X"))
+            {
+                string senzaX = chiave.Substring(1);
+                varianti.Add(senzaX);
+                varianti.UnionWith(GeneraVariantiDiChiave(senzaX));
+            }
+
+            return varianti.ToList();
+        }
+
+        static string CercaNelDizionarioConVarianti(string chiave, Dictionary<string, (string nome, string valore)> dizionario, out string nome)
+        {
+            nome = null;
+            foreach (var variante in GeneraVariantiDiChiave(chiave))
+            {
+                if (dizionario.ContainsKey(variante))
+                {
+                    nome = dizionario[variante].nome;
+                    return variante;
+                }
+            }
+            return null;
+        }
+        static void AggiornaSecondoExcel(string fileOriginale, string fileCopia, string sheetName, Dictionary<string, (string nome, string valore)> dizionario)
+        {
+            File.Copy(fileOriginale, fileCopia, true);
+
+            using (var package = new ExcelPackage(new FileInfo(fileCopia)))
+            {
+                var worksheet = package.Workbook.Worksheets[sheetName];
+                if (worksheet == null) return;
+
+                int rowCount = worksheet.Dimension.Rows;
+                var chiaviTrovate = new HashSet<string>();
+
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    var cellaA = worksheet.Cells[row, 1].Text.Trim();
+                    if (string.IsNullOrEmpty(cellaA)) continue;
+
+                    string nome;
+                    string chiaveTrovata = CercaNelDizionarioConVarianti(cellaA, dizionario, out nome);
+                    worksheet.Cells[row, 2].Value = nome;
+
+                    worksheet.Cells[row, 3].Value = chiaveTrovata != null && int.TryParse(dizionario[chiaveTrovata].valore, out int valoreInt) ? valoreInt : 0;
+
+                    if (chiaveTrovata != null)
+                        chiaviTrovate.Add(chiaveTrovata);
+                }
+
+                int newRow = rowCount + 1;
+                foreach (var chiaveMancante in dizionario.Keys.Except(chiaviTrovate))
+                {
+                    worksheet.Cells[newRow, 1].Value = chiaveMancante;
+                    worksheet.Cells[newRow, 2].Value = dizionario[chiaveMancante].nome;
+                    worksheet.Cells[newRow, 3].Value = int.TryParse(dizionario[chiaveMancante].valore, out int valoreInt) ? valoreInt : 0;
+                    newRow++;
+                }
+
+                package.Save();
+
+            }
+        }
+ 
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                textBox1.AppendText("Avviato");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
